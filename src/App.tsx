3b14d6e5e9626765c33ce73c4f226a046262fdf9
Eduardo da Silva <eduardosilva.pn@gmail.com>
2025-02-15 15:10:42 -0300

    better layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { listen } from "@tauri-apps/api/event";
 import { invoke } from "@tauri-apps/api/core";
+import {
+  TextField,
+  Button,
+  Autocomplete,
+  Card,
+  CardContent,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 import ReactMarkdown from "react-markdown";
 import "./App.css";
 
+type card = {
+  model: string;
+  message: string;
+  end: boolean;
+};
+
 function App() {
-  const [askResponse, setAskResponse] = useState("");
-  const [message, setName] = useState("");
-  const [model, setModel] = useState<undefined | string>(undefined);
+  const [askInResponse, setAskInResponse] = useState("");
+  const [responsesCard, setResponsesCard] = useState<Array<card>>([]);
+  const [messageInput, setMessageInput] = useState("");
+  const [model, setModel] = useState("llama3.2");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    await invoke("ia_ask_stream", { model, message });
+  const modelOptions = ["llama3.2", "deepseek-r1:7b"];
+
+  async function makeAsk() {
+    setMessageInput("");
+    const cacheAskResponse = responsesCard;
+    cacheAskResponse.push({
+      model,
+      message: "",
+      end: false,
+    });
+    setResponsesCard(cacheAskResponse);
+
+    const message = (await invoke("ia_ask_stream", {
+      model,
+      message: messageInput,
+    })) as string;
+
+    const lastIndex = cacheAskResponse.length - 1;
+    cacheAskResponse[lastIndex].end = true;
+    cacheAskResponse[lastIndex].message = message;
+    setResponsesCard(cacheAskResponse);
+    setAskInResponse("");
   }
 
-  async function clean() {
-    setAskResponse("");
-  }
+  useEffect(() => {
+    const container = document.querySelector(".message-container");
+    if (container) container.scrollTop = container.scrollHeight;
+  }, [askInResponse]);
 
   listen("ai_ask_stream_return", (event) => {
     if (
@@ -24,36 +59,56 @@ function App() {
       "message" in event?.payload &&
       typeof event.payload.message === "string"
     ) {
-      setAskResponse(event.payload.message);
+      setAskInResponse(event.payload.message);
     }
   });
 
   return (
     <main className="container">
-      <h1>Rust IA Ask</h1>
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Make a question for ia"
+      <div className="header-container">
+        <Autocomplete
+          size="small"
+          value={model}
+          style={{ width: 200 }}
+          options={modelOptions}
+          renderInput={(params) => <TextField {...params} />}
+          onChange={(_, value) => {
+            if (value) setModel(value);
+          }}
         />
-        <select value={model} onChange={(e) => setModel(e.target.value)}>
-          <option value="llama3.2">llama3.2</option>
-          <option value="deepseek-r1:7b">deepseek-r1:7b</option>
-        </select>
-        <button type="reset" onClick={clean}>
-          Clean
-        </button>
-        <button type="submit">Send</button>
-      </form>
-      <div className="text-container">
-        <ReactMarkdown>{askResponse}</ReactMarkdown>
+      </div>
+      <div className="message-container">
+        <div>
+          {responsesCard.map((response, index) => (
+            <Card key={index}>
+              <CardContent>
+                <div>{response.model}</div>
+                <ReactMarkdown>
+                  {response.end ? response.message : askInResponse}
+                </ReactMarkdown>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <div className="input-container">
+        <TextField
+          id="standard-basic"
+          variant="outlined"
+          placeholder="Ask something"
+          value={messageInput}
+          multiline
+          fullWidth
+          onChange={(e) => setMessageInput(e.currentTarget.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={makeAsk}
+          endIcon={<SendIcon />}
+          style={{ height: "55px" }}
+        >
+          Send
+        </Button>
       </div>
     </main>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          mode: "dark",
+        },
+      })}
+    >
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
